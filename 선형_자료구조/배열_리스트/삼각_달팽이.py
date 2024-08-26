24c707aa4255f0f00de1808a46b70cb08f1c7821
BeomJo Kim <61742009+8eomio@users.noreply.github.com>
2024-08-26 14:42:15 +0900

    Update 삼각_달팽이.py

--- 선형_자료구조/배열_리스트/삼각_달팽이.py
+++ 선형_자료구조/배열_리스트/삼각_달팽이.py
@@ -1,26 +1,21 @@
-def solution(n):
-    dx = [0, 1, -1]
-    dy = [1, 0, -1]
-    arr = [[0]*i for i in range(1, n+1)]
-    x = y = angle = 0
-    cnt = 1
-    size = (n+1)*n //2 
-    
-    while cnt <= size:
-        arr[y][x] = cnt 
-        ny = y + dy[angle]
-        nx = x + dx[angle]
-        cnt += 1
-        if 0<=ny<n and 0 <= nx<= ny and arr[ny][nx]==0:
-            y = ny
-            x = nx
+def solution(n): 
+    dx=[0,1,-1]
+    dy=[1,0,-1]
+    arr=[[0]*i for i in range(1,n+1)]
+    x,y=0,0
+    num=1
+    d=0
+    while num<=(n+1)*n//2:
+        arr[y][x]=num
+        ny=y+dy[d]
+        nx=x+dx[d]
+        num+=1
+        if 0<=ny<n and 0<=nx<=ny and b[ny][nx]==0:
+            y,x=ny,nx
         else:
-            angle = (angle+1)%3
-            y += dy[angle]
-            x += dx[angle]
-        
-        
-    answer = [i for j in arr for i in j]
+            d=(d+1)%3
+            y+=dy[d]
+            x+=dx[d]
+    return sum(arr,[])
     
-    
-    return answer
+

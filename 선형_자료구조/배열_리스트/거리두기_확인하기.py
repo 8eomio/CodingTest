23f8bd472a273983c117ee3db88b01f3ff303d1f
BeomJo Kim <61742009+8eomio@users.noreply.github.com>
2024-08-26 15:30:22 +0900

    Update 거리두기_확인하기.py

--- 선형_자료구조/배열_리스트/거리두기_확인하기.py
+++ 선형_자료구조/배열_리스트/거리두기_확인하기.py
@@ -1,30 +1,21 @@
 def check(place):
-    for idx_row, row in enumerate(place):
-        for idx_col, cell in enumerate(row):
+    for irow, row in enumerate(place):
+        for icol, cell in enumerate(row):
             if cell != 'P':
                 continue
-            if idx_row != 4:
-                D = place[idx_row + 1][idx_col]
-                if D == 'P': return 0 
-                if idx_row < 3:
-                    D2 = place[idx_row+2][idx_col]
-                    if D2 == 'P' and D != 'X': return 0
-                if idx_col != 4:
-                    R = place[idx_row][idx_col + 1]
-                    RD = place[idx_row+1][idx_col + 1]
-                    if RD == 'P' and (D != 'X' or R != 'X'): return 0
-            
-                if idx_col != 0:
-                    L = place[idx_row][idx_col -1]
-                    LD = place[idx_row+1][idx_col - 1]
-                    if LD == 'P' and (D != 'X' or L != 'X'): return 0
-            if idx_col != 4:
-                R = place[idx_row][idx_col+1]
-                if R == 'P': return 0
-                if idx_col < 3: 
-                    R2 = place[idx_row][idx_col+2]
-                    if R2 =='P' and R != 'X': return 0
-                
+            if irow != 4 and place[irow + 1][icol] == 'P':
+                return 0
+            if icol != 4 and place[irow][icol + 1] == 'P':
+                return 0
+            if irow < 3 and place[irow + 2][icol] == 'P' and place[irow + 1][icol] != 'X':
+                return 0
+            if icol < 3 and place[irow][icol + 2] == 'P' and place[irow][icol + 1] != 'X':
+                return 0
+            if irow != 4 and icol != 4 and place[irow + 1][icol + 1] == 'P' and (place[irow + 1][icol] != 'X' or place[irow][icol + 1] != 'X'):
+                return 0
+            if irow != 4 and icol != 0 and place[irow + 1][icol - 1] == 'P' and (place[irow + 1][icol] != 'X' or place[irow][icol - 1] != 'X'):
+                return 0
     return 1
+
 def solution(places):
     return [check(place) for place in places]
